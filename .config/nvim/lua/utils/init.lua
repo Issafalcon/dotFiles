80a31b781341f6d9e46e4a7d73830dddb66c67e7
Adam Figgins <adam_figgins@waters.com>
2021-05-25 17:24:57 +0100

    Removing some unused utils functions to eventually get rid of utils file

--- .config/nvim/lua/utils/init.lua
+++ .config/nvim/lua/utils/init.lua
@@ -1,43 +1,5 @@
 local utils = {}
 
-function utils.define_augroups(definitions) -- {{{1
-  -- Create autocommand groups based on the passed definitions
-  --
-  -- The key will be the name of the group, and each definition
-  -- within the group should have:
-  --    1. Trigger
-  --    2. Pattern
-  --    3. Text
-  -- just like how they would normally be defined from Vim itself
-  for group_name, definition in pairs(definitions) do
-    vim.cmd("augroup " .. group_name)
-    vim.cmd("autocmd!")
-
-    for _, def in pairs(definition) do
-      local command = table.concat(vim.tbl_flatten {"autocmd", def}, " ")
-      vim.cmd(command)
-    end
-
-    vim.cmd("augroup END")
-  end
-end
-
--- misc
-function utils.file_exists(name)
-  local f = io.open(name, "r")
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
-end
-
--- Termcodes escapiing function
-function utils.t(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
 -- Allows easy creation of keymappings
 function utils.map(mode, lhs, rhs, opts)
   local options = {noremap = true}
