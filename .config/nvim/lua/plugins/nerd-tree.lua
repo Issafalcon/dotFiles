51a77ed29513423df74a3795b00b05b25fc016be
Adam Figgins <adam_figgins@waters.com>
2021-05-26 11:39:42 +0100

    Fixing bug with the autocommand in nerd-tree and updating tenancyadmin tmuxinator

--- .config/nvim/lua/plugins/nerd-tree.lua
+++ .config/nvim/lua/plugins/nerd-tree.lua
@@ -1,4 +1,3 @@
-require('nvim_utils')
 require('vimp')
 
 vimp.nnoremap('<C-n>', ":NERDTreeToggle<CR>")
@@ -15,12 +14,3 @@ vim.g.NERDTreeGitStatusIndicatorMapCustom = {
   Clean     = '✔︎',
   Unknown   = '?',
 }
-
--- If another buffer tries to replace NERDTree, put it in the other window, and bring back NERDTree.
-local autocmds = {
-  nerdtree = {
-    {'BufEnter', '*', [[if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 | let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif]]}
-  }
-}
-
-nvim_create_augroups(autocmds)
