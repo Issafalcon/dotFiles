13b355d7efaf098bfbeb49689363d7062cea6604
ukafig <adam_figgins@waters.com>
2021-06-16 19:00:04 +0100

    Adding latex live preview and setting viewer to zathura

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -117,6 +117,7 @@ return require("packer").startup(
 
     -- Wiki and Note taking
     use 'lervag/vimtex'
+    use 'xuhdev/vim-latex-live-preview'
 
     -- Utility
     use {"terrortylor/nvim-comment"}
--- .config/nvim/lua/plugins/wiki.lua
+++ .config/nvim/lua/plugins/wiki.lua
@@ -1,18 +1,1 @@
-local wikiLists = {};
-
-local markdownWiki =   {
-  path = '~/vimwiki/',
-  syntax = 'markdown',
-  ext = '.md'
-};
-
-local default =   {
-  path = '~/vimwiki/',
-  syntax = 'default',
-  ext = '.wiki'
-};
-
-wikiLists[1] = default;
-wikiLists[2] = markdownWiki;
-
-vim.g.vimwiki_list = wikiLists;
+vim.g.livepreview_previewer = 'zathura'
