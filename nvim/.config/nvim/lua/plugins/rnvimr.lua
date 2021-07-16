7c93fff65f5d6f25cfa5135640ececb10e868e2c
issafalcon <atfiggins@gmail.com>
2021-07-17 00:00:21 +0100

    Updating rnvimr config

--- nvim/.config/nvim/lua/plugins/rnvimr.lua
+++ nvim/.config/nvim/lua/plugins/rnvimr.lua
@@ -1,6 +1,11 @@
 -- Make Ranger replace netrw and be the file explorer
--- vim.g.rnvimr_ex_enable = 1
+vim.g.rnvimr_ex_enable = 1
 vim.g.rnvimr_draw_border = 1
-vim.g.rnvimr_pick_enable = 1
+vim.g.rnvimr_enable_picker = 0
 vim.g.rnvimr_bw_enable = 1
+-- Change the border's color
+vim.g.rnvimr_border_attr = {fg=14, bg=-1}
+
+-- Draw border with both
+vim.g.rnvimr_ranger_cmd = 'ranger --cmd="set draw_borders both"'
 vim.api.nvim_set_keymap('n', '-', ':RnvimrToggle<CR>', {noremap = true, silent = true})
