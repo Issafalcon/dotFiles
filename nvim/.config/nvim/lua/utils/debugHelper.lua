6e95682732c521054ff5024e23079adac80b9084
issafalcon <atfiggins@gmail.com>
2021-08-03 21:28:35 +0100

    Fixing dap for chrome launch

--- nvim/.config/nvim/lua/utils/debugHelper.lua
+++ nvim/.config/nvim/lua/utils/debugHelper.lua
@@ -1,31 +1,41 @@
-local dap = require('dap')
+local dap = require("dap")
 
 local function debugInChrome()
   local launchUrl = vim.fn.input("Launch URL - Full path or relative to http://localhost: ")
 
-  if string.sub(launchUrl, 1, 1) == '/' then
-    launchUrl = 'http://localhost' .. launchUrl
+  if string.sub(launchUrl, 1, 1) == "/" then
+    launchUrl = "http://localhost" .. launchUrl
   end
 
-  dap.run({
-    type = 'chrome',
-    request = 'launch',
-    stopOnEntry = true,
-    cwd = vim.fn.getcwd(),
-    url = launchUrl,
-    webRoot = '${workspaceFolder}',
-    runtimeExecutable = '/usr/bin/google-chrome'
-  })
+  local sourcemapKeys = { "/./*", "/src/*", "webpack:///./*", }
+  local webrootMapPath = "${webRoot}/*"
+  local sourceMapOverrides = {}
+  sourceMapOverrides[sourcemapKeys[1]] = webrootMapPath
+  sourceMapOverrides[sourcemapKeys[2]] = webrootMapPath
+  sourceMapOverrides[sourcemapKeys[3]] = webrootMapPath
+       -- "/*": "*",
+       -- "/./~/*": "${webRoot}/node_modules/*"
+  dap.run(
+    {
+      type = "chrome",
+      request = "launch",
+      stopOnEntry = true,
+      url = launchUrl,
+      webRoot = "${workspaceFolder}",
+      runtimeExecutable = "/usr/bin/google-chrome",
+      sourceMapPathOverrides = sourceMapOverrides
+    }
+  )
 end
 
 local function startDebugging()
- if dap.session() then
-   dap.disconnect()
-   dap.close()
+  if dap.session() then
+    dap.disconnect()
+    dap.close()
   end
 
-  if vim.bo.filetype == 'typescript' then
-   debugInChrome()
+  if vim.bo.filetype == "typescript" then
+    debugInChrome()
   end
 end
 
