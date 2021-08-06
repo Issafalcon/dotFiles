9ca6a7f4f8251a08ab6f50606c370b2edcb1125e
Adam Figgins <adam_figgins@waters.com>
2021-08-06 11:59:19 +0100

    Adding hop keybindings

--- nvim/.config/nvim/lua/plugins/hop.lua
+++ nvim/.config/nvim/lua/plugins/hop.lua
@@ -1,1 +1,9 @@
 require("hop").setup()
+require("vimp")
+
+-- place this in one of your configuration file(s)
+vimp.nnoremap('<leader><leader>w', "<cmd>lua require'hop'.hint_words()<cr>")
+vimp.nnoremap('<leader><leader>1', "<cmd>lua require'hop'.hint_char1()<cr>")
+vimp.nnoremap('<leader><leader>2', "<cmd>lua require'hop'.hint_char2()<cr>")
+vimp.nnoremap('<leader><leader>/', "<cmd>lua require'hop'.hint_patterns()<cr>")
+vimp.nnoremap('<leader><leader>l', "<cmd>lua require'hop'.hint_lines()<cr>")
