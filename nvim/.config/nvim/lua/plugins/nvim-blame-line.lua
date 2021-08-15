4b54baee768cd23cd93be24e20daecfa6632b65c
Adam Figgins <atfiggins@gmail.com>
2021-08-15 14:26:48 +0100

    Removing auto-on for blame-line on buffer enter
    - It gets annoying after a while. Better to just toggle

--- nvim/.config/nvim/lua/plugins/nvim-blame-line.lua
+++ nvim/.config/nvim/lua/plugins/nvim-blame-line.lua
@@ -1,14 +1,5 @@
 require('vimp')
-require('nvim_utils')
 
 vim.g.blameLineVirtualTextHighlight = 'Question'
 
 vimp.nnoremap({silent = true }, '<leader>gb', ':ToggleBlameLine<CR>')
-
-local autoCmds = {
-  blameLine = {
-    { "BufEnter", "*", "EnableBlameLine" };
-  };
-}
-
-nvim_create_augroups(autoCmds)
