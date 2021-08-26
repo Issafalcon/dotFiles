d645cbfc15edf9a7389f9cf27950175fcfebe280
Adam Figgins <atfiggins@gmail.com>
2021-08-26 20:04:43 +0100

    Removing install script from fzf config

--- fzf/config.zsh
+++ fzf/config.zsh
@@ -1,10 +1,5 @@
 #!/bin/zsh
 
-brew install fzf
-
-# To install useful key bindings and fuzzy completion:
-$(brew --prefix)/opt/fzf/install
-
 # Uses zinit fzf package and adds the man pages from fzf
 # Also installs fzf-tmux
 zinit pack=bgn atclone="cp fzy.1 $ZPFX/man/man1" for fzy
