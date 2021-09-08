19820ab97f531132855aea1e7cf85d38ee73c1aa
Adam Figgins <adam_figgins@waters.com>
2021-09-08 16:24:39 +0100

    Reverting change to profile. Doesn't work

--- gojira/completion.zsh
+++ gojira/completion.zsh
@@ -1,3 +1,3 @@
 # This doesn't seem to work when sourced from zshrc. Needs to be run after prompt is loaded
-eval "$(jira --completion-script-zsh)"
+eval "$(/usr/bin/jira --completion-script-zsh)"
 
