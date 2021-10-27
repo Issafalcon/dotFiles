b8aa8c472bc810c53c6cb6b49719c197275348d0
Adam Figgins <adam_figgins@waters.com>
2021-10-27 16:57:22 +0100

    Nvim - Updating the custom_attach for key bindings

--- nvim/.config/nvim/lua/lsp/custom_attach.lua
+++ nvim/.config/nvim/lua/lsp/custom_attach.lua
@@ -17,6 +17,9 @@ local function custom_attach(client)
   if vim.bo.filetype == "cs" then
     vimp.add_buffer_maps(
       function()
+
+        -- Mapping for functionas that Omnisharp_vim does better than the LSP
+        -- This seems to be changing with the improvements to omnisharp LSP
         vimp.nmap({"silent"}, "gd", "<Plug>(omnisharp_go_to_definition)")
         vimp.nmap({"silent"}, "gr", "<Plug>(omnisharp_find_usages)")
         vimp.nmap({"silent"}, "<leader>gi", "<Plug>(omnisharp_find_implementations)")
@@ -24,11 +27,6 @@ local function custom_attach(client)
         vimp.nmap({"silent"}, "<leader>gpi", "<Plug>(omnisharp_preview_implementations)")
         vimp.nmap({"silent"}, "gtd", "<Plug>(omnisharp_type_lookup)")
         vimp.nmap({"silent"}, "<leader>ac", "<Plug>(omnisharp_global_code_check)")
-        vimp.nmap({"silent"}, "K", ":Lspsaga hover_doc<CR>")
-        -- vimp.nmap({"silent"}, "K", "<Plug>(omnisharp_documentation)") -- Doesn't work well with Omnisharp_vim
-        vimp.nmap({"silent"}, "rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-        -- vimp.nmap({"silent"}, "rn", "<Plug>(omnisharp_rename)") -- Doesn't work anymore for Omnisharp_vim
-        vimp.nmap({"silent"}, "<leader>f", "<Plug>(omnisharp_code_format)")
         vimp.nmap({"silent"}, "gm", "<Plug>(omnisharp_find_members)")
       end
     )
@@ -39,33 +37,26 @@ local function custom_attach(client)
     buf_set_keymap("n", "<leader>gi", ":lua require('telescope.builtin').lsp_implementations()<CR>", opts)
     buf_set_keymap('n', "gr", ":lua require('telescope.builtin').lsp_references()<CR>", opts)
     buf_set_keymap('n', "gm", ":lua require('telescope.builtin').lsp_document_symbols()<CR>", opts)
-    buf_set_keymap("n", "rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
     buf_set_keymap("n", "<Leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-
-    buf_set_keymap("n", "K", ":Lspsaga hover_doc<CR>", opts)
-
-    if client.resolved_capabilities.document_formatting then
-      buf_set_keymap("n", "<Leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    end
-
-    if client.resolved_capabilities.document_range_formatting then
-      buf_set_keymap("v", "<Leader>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-    end
+    buf_set_keymap("n", "<leader>ac", ":lua require('telescope.builtin').lsp_workspace_diagnostics()<CR>", opts)
   end
 
-  -- LSP Signature
+  if client.resolved_capabilities.document_formatting then
+    buf_set_keymap("n", "<Leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  end
+
+  if client.resolved_capabilities.document_range_formatting then
+    buf_set_keymap("v", "<Leader>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+  end
+
+  buf_set_keymap("n", "K", ":Lspsaga hover_doc<CR>", opts)
+  buf_set_keymap("n", "rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
   buf_set_keymap("n", "<A-s>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-
-  -- Telescope Pickers
   buf_set_keymap("n", "<leader>ca", ":lua require('telescope.builtin').lsp_code_actions()<CR>", opts)
-  buf_set_keymap("n", "<leader>ac", ":lua require('telescope.builtin').lsp_workspace_diagnostics()<CR>", opts)
   buf_set_keymap("n", "<leader>sd", ":lua require('telescope.builtin').lsp_definitions()<CR>", opts)
-
-  -- LspSaga
   buf_set_keymap("n", "<leader>gh", ":Lspsaga lsp_finder<CR>", opts)
   buf_set_keymap("n", "[g", ":Lspsaga diagnostic_jump_prev<CR>", opts)
   buf_set_keymap("n", "]g", ":Lspsaga diagnostic_jump_next<CR>", opts)
-  -- buf_set_keymap("n", "<leader>ca", ":Lspsaga code_action<CR>", opts)
   buf_set_keymap("n", "<C-f>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", opts)
   buf_set_keymap("n", "<C-b>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", opts)
 
