ee2cbe708c77d6f33dba074d66c0afdcd39dbaf1
Adam Figgins <atfiggins@gmail.com>
2021-10-29 20:52:55 +0100

    Extending typescript files in emmet

--- nvim/.config/nvim/lua/plugins/emmet-vim.lua
+++ nvim/.config/nvim/lua/plugins/emmet-vim.lua
@@ -1,2 +1,2 @@
 vim.g.user_emmet_leader_key='<C-e>'
-vim.cmd('let g:user_emmet_settings = { "javascript.jsx" : { "extends" : "jsx" }}')
+vim.cmd('let g:user_emmet_settings = { "javascript.jsx" : { "extends" : "jsx" }, "typescript.tsx" : { "extends" : "tsx" }}')
