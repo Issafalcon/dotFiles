a216b686599d0640141da25d6c672951180b79ae
Adam Figgins <adam_figgins@waters.com>
2021-11-03 11:59:37 +0000

    Adding omnisharp key mappings for running / debugging tests

--- nvim/.config/nvim/lua/plugins/omnisharp-vim.lua
+++ nvim/.config/nvim/lua/plugins/omnisharp-vim.lua
@@ -1,3 +1,5 @@
+require("vimp")
+
 vim.g.OmniSharp_highlight_groups = {
     ClassName = 'ClassName',
     StaticSymbol = 'StaticSymbol',
@@ -17,3 +19,7 @@ vim.g.OmniSharp_highlight_groups = {
     EventName = 'EventName'
 }
 
+-- Mappings for omnisharp only functions (functions not available in LSP)
+vimp.nmap({"silent"}, "<leader>odt", "<Plug>(omnisharp_debug_test)")
+vimp.nmap({"silent"}, "<leader>ort", "<Plug>(omnisharp_run_test)")
+vimp.nmap({"silent"}, "<leader>ot", "<Plug>(omnisharp_run_tests_in_file)")
