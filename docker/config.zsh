238d05998c621543d91ef398670a0951e8e3d31c
Adam Figgins <atfiggins@gmail.com>
2021-11-29 22:09:21 +0000

    Adding docker module with autocompletion plugins

---
+++ docker/config.zsh
@@ -0,0 +1,3 @@
+# Install the oh-my-zsh docker and docker compose plugins as a zinit snippet
+zinit snippet OMZ::plugins/docker/_docker
+zinit snippet OMZ::plugins/docker-compose/_docker-compose
---
+++ docker/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Taken from https://docs.docker.com/engine/install/ubuntu/
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Update the apt package index and install packages needed to use docker
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Add dockers official dpg key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the stable repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install docker engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
