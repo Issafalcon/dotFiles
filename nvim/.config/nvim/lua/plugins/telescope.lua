cefc65d06f6d7ee64449995537d32c91befabeb3
Adam Figgins <adam_figgins@waters.com>
2021-12-03 17:01:57 +0000

    Updating cs vimspector conf and telescope layout

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -52,7 +52,7 @@ require("telescope").setup {
     file_previewer = require "telescope.previewers".vim_buffer_cat.new,
     grep_previewer = require "telescope.previewers".vim_buffer_vimgrep.new,
     qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
-    layout_strategy = "horizontal",
+    layout_strategy = "vertical",
     layout_config = {
       vertical = {
         height = {
