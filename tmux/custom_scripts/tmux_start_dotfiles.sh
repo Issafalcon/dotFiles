c4879fc5f4ee79709c1e5c91d28e62ef015d3a49
Adam Figgins <atfiggins@gmail.com>
2022-01-04 18:40:44 +0000

    Tmux - Updating dotfiles startup

--- tmux/custom_scripts/tmux_start_dotfiles.sh
+++ tmux/custom_scripts/tmux_start_dotfiles.sh
@@ -7,14 +7,18 @@ TMUX=$(tmux new-session -c ~/dotFiles -d -s dotfiles -n editor)
 tmux send-keys -t dotfiles:1.0 nvim C-m
 
 # Create other windows.
-tmux new-window -c ~/dotFiles -t dotfiles:2 -n Terminals
+tmux new-window -c "${DOTFILES}"/nvim/.config/nvim -t dotfiles:2 -n nvim-config
+
+tmux send-keys -t dotfiles:2.0 nvim C-m
+
+tmux new-window -c ~/dotFiles -t dotfiles:3 -n Terminals
 
 tmux select-layout -t dotfiles:1 tiled
 tmux select-layout -t dotfiles:2 tiled
 
-tmux select-pane -t dotfiles:2.0
-tmux select-window -t dotfiles:editor
-tmux select-pane -t dotfiles:editor.0
+
+tmux select-window -t dotfiles:nvim-config
+tmux select-pane -t dotfiles:nvim-config.0
 
 if [ -z "$TMUX" ]; then
   tmux -u attach-session -t dotfiles
