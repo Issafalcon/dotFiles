907eacd24ad148eb9680f00797436af795f44ec1
Adam Figgins <adam_figgins@waters.com>
2022-01-12 10:33:45 +0000

    Go - Updating version of go install
    - Previous version used deprecated functions when installing efm-ls

--- go/install.sh
+++ go/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget https://dl.google.com/go/go1.17.5.linux-amd64.tar.gz
-sudo tar -C /usr/local/ -xzf go1.17.5.linux-amd64.tar.gz
-rm -f go1.17.5.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+wget https://dl.google.com/go/go1.17.6.linux-amd64.tar.gz
+sudo tar -C /usr/local/ -xzf go1.17.6.linux-amd64.tar.gz
+rm -f go1.17.6.linux-amd64.tar.gz
  
