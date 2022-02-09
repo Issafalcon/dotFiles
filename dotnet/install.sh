0c12f44ff5b838483231092193664184073fa403
Adam Figgins <adam_figgins@waters.com>
2022-02-09 15:57:53 +0000

    Dotnet - Adding analyzer support back to omnisharp
    - Updating editorconfig to hopefully get rid of editor warnings
    - Adding xunit cli for testing

--- dotnet/install.sh
+++ dotnet/install.sh
@@ -9,3 +9,6 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https \
 	&& sudo apt-get update \
 	&& sudo apt-get install -y dotnet-sdk-6.0
+
+dotnet tool install --global xunit-cli --version 0.1.16
+
---
+++ dotnet/path.zsh
@@ -0,0 +1,1 @@
+path+=($HOME/.dotnet/tools)
