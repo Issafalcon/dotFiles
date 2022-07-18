c9aa98e3f4deb43917f2aad738b43d01e63b5fb5
Adam Figgins <adam.figgins@nweh.co.uk>
2022-07-18 17:28:31 +0100

    Chrome - Adding chrome install module

---
+++ googlechrome/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update -y &&
+	sudo apt-get upgrade -y
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
