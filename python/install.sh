6d2d4fd58ba79b863cb3449fa543d817acf4295d
Adam Figgins <adam.figgins@nweh.co.uk>
2022-08-05 15:20:12 +0100

    Python and nvim - Adding python venv install
    - Nvim updates to make loading plugins more robust in various
      environments

--- python/install.sh
+++ python/install.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.9 python3-pip python-dev python3-dev
+sudo apt install python3.9 python3-pip python-dev python3-dev python3.8-venv
 
 echo Python version is "$(python3.9 --version)"
