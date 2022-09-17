e25d5d3003861c1d0c9af4b90f86e6b1bd660589
Adam Figgins <atfiggins@gmail.com>
2022-09-17 20:59:41 +0100

    Dotnet - Updating dotnet install commands

--- dotnet/install.sh
+++ dotnet/install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-
-# Download and install the automated dotnet install script from mcr to install .NET 5.0
-wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-
-sudo apt-get update
-sudo apt-get install -y apt-transport-https \
-	&& sudo apt-get update \
-	&& sudo apt-get install -y dotnet-sdk-6.0
-
-dotnet tool install --global xunit-cli --version 0.1.16
-
+# Install individual components due to issue with package mapping
+# see https://github.com/dotnet/sdk/issues/27082#issuecomment-1211143446 for details
+sudo apt install \
+  aspnetcore-runtime-6.0=6.0.8-1 \
+  dotnet-apphost-pack-6.0=6.0.8-1 \
+  dotnet-host=6.0.8-1 \
+  dotnet-hostfxr-6.0=6.0.8-1 \
+  dotnet-runtime-6.0=6.0.8-1 \
+  dotnet-sdk-6.0=6.0.400-1 \
+  dotnet-targeting-pack-6.0=6.0.8-1
--- dotnet/path.zsh
+++
@@ -1,1 +0,0 @@
-path+=($HOME/.dotnet/tools)
