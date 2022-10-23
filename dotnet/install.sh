24698fc92da2238108c8cd55b5fc7ac45143a0f2
Adam Figgins <atfiggins@gmail.com>
2022-10-23 14:26:06 +0100

    Dotnet - Update install

--- dotnet/install.sh
+++ dotnet/install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# Install individual components due to issue with package mapping
-# see https://github.com/dotnet/sdk/issues/27082#issuecomment-1211143446 for details
-sudo apt install \
-  aspnetcore-runtime-6.0=6.0.8-1 \
-  dotnet-apphost-pack-6.0=6.0.8-1 \
-  dotnet-host=6.0.8-1 \
-  dotnet-hostfxr-6.0=6.0.8-1 \
-  dotnet-runtime-6.0=6.0.8-1 \
-  dotnet-sdk-6.0=6.0.400-1 \
-  dotnet-targeting-pack-6.0=6.0.8-1
+# Check if using Ubuntu 22.04
+if [ "$(lsb_release -rs)" = "22.04" ]; then
+	sudo apt update &&
+		sudo apt install dotnet-sdk-6.0
+  else
+    echo "Not on Ubuntu 22.04. Please refer to https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu?source=recommendations"
+fi
