d9ed697766c19f53cb601131d745014cc737fca5
Adam Figgins <adam.figgins@gmail.com>
2022-12-18 19:26:23 +0000

    chore(python): Updating python dependencies

--- python/install.sh
+++ python/install.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.10 python3-pip python-dev python3-dev python3.10-venv
+sudo apt install python3.10 python3-pip python3-dev python3.10-venv
 
 echo Python version is "$(python3.9 --version)"
