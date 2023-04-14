7e4aacd48401e135bd873d9b43106e1861741487
Adam Figgins <atfiggins@gmail.com>
2023-04-14 22:18:01 +0100

    fix(wezterm): Uses wezterm-nightly instead

--- wezterm/install.sh
+++ wezterm/install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-curl -LO https://github.com/wez/wezterm/releases/download/20230326-111934-3666303c/wezterm-20230326-111934-3666303c.Ubuntu22.04.deb
-sudo apt install -y ./wezterm-20230326-111934-3666303c.Ubuntu22.04.deb
+curl -LO https://github.com/wez/wezterm/releases/download/nightly/wezterm-nightly.Ubuntu22.04.deb
+sudo apt install -y ./wezterm-nightly.Ubuntu22.04.deb
 
-if [ ! -d ~/bin ]; then
-    mkdir ~/bin
-fi
-
-mv ./WezTerm-20230408-112425-69ae8472-Ubuntu20.04.AppImage ~/bin/wezterm
+rm wezterm-nightly.Ubuntu22.04.deb
