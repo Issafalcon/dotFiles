2c857672350c9203a5daf8702b3774b1d5833b1c
Adam Figgins <adam.figgins@gmail.com>
2023-04-16 14:20:35 +0100

    chore(neotest-tmux): Updates tmux script for neotest-dotnet project

--- tmux/custom_scripts/tmux_start_neotest_dotnet.sh
+++ tmux/custom_scripts/tmux_start_neotest_dotnet.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Check if wiki dir exists and create it if not.
 if [ ! -d "${PROJECTS}"/neotest-dotnet ]; then
-
   # Clone repo into dir.
   git clone https://github.com/Issafalcon/neotest-dotnet.git "${PROJECTS}"/neotest-dotnet
+fi
+
+if [ ! -d "${PROJECTS}"/neotest ]; then
+  # Clone repo into dir.
+  git clone https://github.com/nvim-neotest/neotest.git "${PROJECTS}"/neotest
 fi
 
 # Create the session and the first window. Manually switch to root
@@ -13,7 +16,11 @@ TMUX=$(tmux new-session -c "${PROJECTS}"/neotest-dotnet -d -s neotest-dotnet -n 
 
 tmux send-keys -t neotest-dotnet:1.0 nvim C-m
 
-tmux select-layout -t neotest-dotnet:1 tiled
+# neotest core
+tmux new-window -c "${PROJECTS}"/neotest -t neotest-dotnet:2 -n neotest
+tmux send-keys -t neotest-dotnet:neotest nvim C-m
+
+tmux select-window -t neotest-dotnet:neotest-dotnet
 tmux select-pane -t neotest-dotnet:1.0
 
 if [ -z "$TMUX" ]; then
