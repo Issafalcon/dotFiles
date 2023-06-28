fed53fbba5923c74e8699674ea2976c225bc78d9
Adam Figgins <adam.figgins@wedo-digital.co.uk>
2023-06-28 12:15:53 +0100

    fix(docker): Fixes the loading of zinit OMZ docker plugins
    - Docker completions moved location, so this loads completions rather
      than aliases

--- docker/config.zsh
+++ docker/config.zsh
@@ -1,3 +1,8 @@
 # Install the oh-my-zsh docker and docker compose plugins as a zinit snippet
-zinit snippet OMZ::plugins/docker/_docker
+
+# Just the completion snippets for docker (the OMZ plugin tries to load this snippet file from the ZSH_CACHE location, which won't be found)
+zinit snippet https://github.com/docker/cli/blob/master/contrib/completion/zsh/_docker
+
+# Docker compose plugin (with all the aliases) and the completion script for docker-compose
+zinit snippet OMZ::plugins/docker-compose/docker-compose.plugin.zsh
 zinit snippet OMZ::plugins/docker-compose/_docker-compose
