ebb971bbd551177e6382d703741ce8ab41d30f8d
Adam Figgins <adam.figgins@wedo-digital.co.uk>
2023-07-13 16:14:38 +0100

    fix(azure): Removes redundant autoload from completion.zsh

--- azure/completion.zsh
+++ azure/completion.zsh
@@ -1,8 +1,4 @@
 #!/bin/zsh
 
-# Get the bash completions for azure cli
-autoload -U +X compinit && compinit
-autoload -U +X bashcompinit && bashcompinit
-
 # TODO: Figure out why this doesn't source properly
 source /etc/bash_completion.d/azure-cli
