dc7c8a4f1668bb5a56e3b48ad5b2af2d80f9e3c2
Adam Figgins <adam@pop-os.localdomain>
2023-10-04 20:27:33 +0100

    chore(dotnet): Update dotnet version to 7

--- dotnet/install.sh
+++ dotnet/install.sh
@@ -2,7 +2,7 @@
 # Check if using Ubuntu 22.04
 if [ "$(lsb_release -rs)" = "22.04" ]; then
 	sudo apt update -y &&
-		sudo apt install -y dotnet-sdk-6.0
+		sudo apt install -y dotnet-sdk-7.0
   else
     echo "Not on Ubuntu 22.04. Please refer to https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu?source=recommendations"
 fi
