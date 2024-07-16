978e3543954e370a6fb3e71a150ee0c8a8dc1952
Adam Figgins <adam.figgins@wedo-digital.co.uk>
2024-07-16 17:01:41 +0100

    feat(dotnet): Adds dotnet OMZ plugin

--- dotnet/config.zsh
+++ dotnet/config.zsh
@@ -1,1 +1,4 @@
 export DOTNET_ROOT=/usr/share/dotnet
+
+# OMZ dotnet plugin - https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/dotnet/dotnet.plugin.zsh
+zinit snippet OMZ::plugins/dotnet/dotnet.plugin.zsh
