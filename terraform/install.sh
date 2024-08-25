db10161fc0e88c9ef6d435420cd898f4dd14c6cd
Adam Figgins <atfiggins@gmail.com>
2024-08-25 16:42:33 +0100

    feat(tf): Terraform install updates

--- terraform/install.sh
+++ terraform/install.sh
@@ -4,6 +4,6 @@ sudo apt update && sudo apt install -y gnupg software-properties-common curl
 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-
-sudo apt update && sudo apt install  -y terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install -y terraform
