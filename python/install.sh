dd5713eb40e2166c9507e65d1bb81aff3bfaa3ec
Adam Figgins <atfiggins@gmail.com>
2024-11-12 16:29:20 +0000

    Python install script updates
    - More generic for latest v3 versions

--- python/install.sh
+++ python/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo apt update
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.13 python3-pip python3-dev python3-venv
+sudo apt install python3
+sudo apt install python3-pip 
+sudo apt install python3-dev
+sudo apt install python3-venv
 
-echo Python version is "$(python3.9 --version)"
+echo Python version is "$(python3 --version)"
