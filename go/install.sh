8af5bc6fec1acec9977d3aa9402d652657e6fed1
ukafig <adam_figgins@waters.com>
2024-11-12 15:56:55 +0000

    Go - updating install version

--- go/install.sh
+++ go/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo rm -rf /usr/local/go
-wget https://dl.google.com/go/go1.17.6.linux-amd64.tar.gz
-sudo tar -C /usr/local/ -xzf go1.17.6.linux-amd64.tar.gz
-rm -f go1.17.6.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.23.3.linux-amd64.tar.gz
+sudo tar -C /usr/local/ -xzf go1.23.3.linux-amd64.tar.gz
+rm -f go1.23.3.linux-amd64.tar.gz
  
