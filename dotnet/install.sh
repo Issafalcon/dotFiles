1a66e3cb8269c5a0feccb0fcc9d927243cc0d116
Adam Figgins <adam_figgins@waters.com>
2025-02-26 16:34:59 +0000

    Dotnet - Install .net 9.0

--- dotnet/install.sh
+++ dotnet/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# Check if using Ubuntu 22.04
+# Check minimum version of ubuntu 20.04
 sudo apt update -y &&
   sudo apt install -y dotnet-sdk-8.0
+
 dotnet tool install --global PlantUmlClassDiagramGenerator
+
+if [ $(lsb_release -rs) == "20.04" ]; then
+  sudo add-apt-repository ppa:dotnet/backports
+fi
+
+sudo apt install -y dotnet-sdk-9.0
