32f36bbffa185bfed3ef26cac9ce622d87493f21
Adam Figgins <atfiggins@gmail.com>
2025-03-20 20:22:14 +0000

    Obsidian - Update version

--- obsidian/install.sh
+++ obsidian/install.sh
@@ -2,5 +2,5 @@
 
 sudo apt update -y
 sudo apt install libnss3-dev
-sudo curl -Lo /usr/bin/obsidian https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.7/Obsidian-1.6.7.AppImage
+sudo curl -Lo /usr/bin/obsidian https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.9/Obsidian-1.8.9.AppImage
 sudo chmod 777 /usr/bin/obsidian
