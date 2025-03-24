cf0fadea3505b46479d5e25cafa7f60f1f2a9d55
Adam Figgins <adam_figgins@waters.com>
2025-03-24 14:11:12 +0000

    QMK - Adding QMK module to install

---
+++ qmk/completion.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+if [[ -r "${PROJECTS}/qmk_firmware/util/qmk_tab_complete.sh" ]]; then
+  source "${PROJECTS}/qmk_firmware/util/qmk_tab_complete.sh"
+fi
---
+++ qmk/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Need python and pip to install below
+if command -v python3 >/dev/null; then
+  echo "Python 3 found. Skipping python 3 installation"
+else
+  "${SCRIPT_DIR}"/../bootstrap.sh "-i" "-m" "python"
+  path+=(/usr/bin/pip3)
+fi
+
+if [[ ! -d "$HOME/python3/envs/qmk" ]]; then
+  mkdir -p "$HOME"/python3/envs
+  cd "$HOME"/python3/envs || exit
+  python3 -m venv qmk
+  source "$HOME"/python3/envs/qmk/bin/activate
+  python3 -m pip install qmk
+  deactivate
+fi
