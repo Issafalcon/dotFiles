9bf1a305f4d3b72d2a0c662042c510760543cd5c
Adam Figgins <adam_figgins@waters.com>
2025-03-27 11:11:49 +0000

    Localstack - Updating version install and py-env setup

--- localstack/install.sh
+++ localstack/install.sh
@@ -1,7 +1,25 @@
-curl --output localstack-cli-3.8.0-linux-amd64-onefile.tar.gz \
-    --location https://github.com/localstack/localstack-cli/releases/download/v3.8.0/localstack-cli-3.8.0-linux-amd64-onefile.tar.gz
+curl --output localstack-cli-4.3.0-linux-amd64-onefile.tar.gz \
+  --location https://github.com/localstack/localstack-cli/releases/download/v4.3.0/localstack-cli-4.3.0-linux-amd64-onefile.tar.gz
 
-sudo tar xvzf localstack-cli-3.8.0-linux-*-onefile.tar.gz -C /usr/local/bin
+sudo tar xvzf localstack-cli-4.3.0-linux-*-onefile.tar.gz -C /usr/local/bin
 
 # Remove the downloaded tarball
-rm localstack-cli-3.8.0-linux-*-onefile.tar.gz
+rm localstack-cli-4.3.0-linux-*-onefile.tar.gz
+
+# Setup awslocal
+# Need python and pip to install below
+if command -v python3 >/dev/null; then
+  echo "Python 3 found. Skipping python 3 installation"
+else
+  "${SCRIPT_DIR}"/../bootstrap.sh "-i" "-m" "python"
+  path+=(/usr/bin/pip3)
+fi
+
+if [[ ! -d "$HOME/python3/envs/awslocal" ]]; then
+  mkdir -p "$HOME"/python3/envs
+  cd "$HOME"/python3/envs || exit
+  python3 -m venv awslocal
+  source "$HOME"/python3/envs/awslocal/bin/activate
+  python3 -m pip install awscli-local
+  deactivate
+fi
