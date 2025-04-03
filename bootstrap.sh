ce8e0f052d2d890747e84c48de0cd3bc189385e5
Adam Figgins <adam@pop-os.localdomain>
2025-04-03 21:41:16 +0100

    Bootstrap - Adds additional install args to pass on

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,122 +12,131 @@ eval set -- "$ARGS"
 set -e
 
 while true; do
-	case "$1" in
-		-m | --module)
-			MODULE="${2}"
-			shift 2
-			;;
-		-i | --install)
-			INSTALL=true
-			shift
-			;;
-		-u | --uninstall)
-			UNINSTALL=true
-			shift
-			;;
-		-r | --remove)
-			REMOVE=true
-			shift
-			;;
-		--)
-			break
-			;;
-	esac
+  case "$1" in
+  -m | --module)
+    MODULE="${2}"
+    shift 2
+    ;;
+  -i | --install)
+    INSTALL=true
+    shift
+    ;;
+  -u | --uninstall)
+    UNINSTALL=true
+    shift
+    ;;
+  -r | --remove)
+    REMOVE=true
+    shift
+    ;;
+  --)
+    break
+    ;;
+  esac
 done
 
 echo ''
 
+# Now look for arguments set after the -- in the call to this script
+shift $((OPTIND - 1))
+EXTRA_ARGS=("$@")
+
+# Skip the "--" argument
+if [[ ${#EXTRA_ARGS[@]} -gt 0 ]]; then
+  unset "EXTRA_ARGS[0]"
+fi
+
 info() {
-	# shellcheck disable=SC2059
-	printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+  # shellcheck disable=SC2059
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
 }
 
 user() {
-	# shellcheck disable=SC2059
-	printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+  # shellcheck disable=SC2059
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
 }
 
 success() {
-	# shellcheck disable=SC2059
-	printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+  # shellcheck disable=SC2059
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
 }
 
 fail() {
-	# shellcheck disable=SC2059
-	printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-	echo ''
-	exit
+  # shellcheck disable=SC2059
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
 }
 
 find_shell() {
-	if command -v "${1}" >/dev/null 2>&1 && grep "$(command -v "${1}")" /etc/shells >/dev/null; then
-		command -v "$1"
-	else
-		echo "/bin/$1"
-	fi
+  if command -v "${1}" >/dev/null 2>&1 && grep "$(command -v "${1}")" /etc/shells >/dev/null; then
+    command -v "$1"
+  else
+    echo "/bin/$1"
+  fi
 }
 
 update_module_config() {
-	if [[ $REMOVE == true ]]; then
+  if [[ $REMOVE == true ]]; then
 
-		sed -i "/$MODULE/d" ~/.dotFileModules \
-			&& stow -D "${MODULE}"
+    sed -i "/$MODULE/d" ~/.dotFileModules &&
+      stow -D "${MODULE}"
 
-		if [[ $? -eq 0 ]]; then
-			success "Successfully removed and unstowed $MODULE module"
-		else
-			fail "Failed to remove and unstow $MODULE module"
-		fi
-		
-		# Swap shell back to bash if we are removing zsh module
-		if [[ ${MODULE} == "zsh" ]]; then
-			BASH="$(find_shell bash)"
-			command -v chsh >/dev/null 2>&1 \
-				&& chsh -s "$BASH" \
-				&& success "set $("$BASH" --version) at $BASH as default shell"
-		fi
-	else
-		if (! grep -q "^${MODULE}$" "$HOME"/.dotFileModules) && [[ "${MODULE}" != "zsh" ]]; then
-			echo "${MODULE}" >>"${HOME}"/.dotFileModules
-		fi
+    if [[ $? -eq 0 ]]; then
+      success "Successfully removed and unstowed $MODULE module"
+    else
+      fail "Failed to remove and unstow $MODULE module"
+    fi
 
-		stow "${MODULE}"
+    # Swap shell back to bash if we are removing zsh module
+    if [[ ${MODULE} == "zsh" ]]; then
+      BASH="$(find_shell bash)"
+      command -v chsh >/dev/null 2>&1 &&
+        chsh -s "$BASH" &&
+        success "set $("$BASH" --version) at $BASH as default shell"
+    fi
+  else
+    if (! grep -q "^${MODULE}$" "$HOME"/.dotFileModules) && [[ "${MODULE}" != "zsh" ]]; then
+      echo "${MODULE}" >>"${HOME}"/.dotFileModules
+    fi
 
-		if [[ $? -eq 0 ]]; then
-			success "Successfully stowed $MODULE module"
+    stow "${MODULE}"
+
+    if [[ $? -eq 0 ]]; then
+      success "Successfully stowed $MODULE module"
 
       if [[ ${MODULE} == "zsh" ]]; then
         zsh
         zinit self-update
       fi
-		else
-			fail "Failed to stow $MODULE module"
-		fi
-	fi
+    else
+      fail "Failed to stow $MODULE module"
+    fi
+  fi
 }
 
 install_module_dependencies() {
-	if [[ -f ./"${MODULE}"/install.sh ]]; then
-		./"${MODULE}"/install.sh
-		if [[ $? -eq 0 ]]; then
+  if [[ -f ./"${MODULE}"/install.sh ]]; then
+    ./"${MODULE}"/install.sh "${EXTRA_ARGS[@]}"
 
-			success "Successfully installed dependencies for $MODULE module"
-		else
-			fail "Failed to install dependencies for $MODULE module"
-		fi
-	fi
+    if [[ $? -eq 0 ]]; then
+      success "Successfully installed dependencies for $MODULE module"
+    else
+      fail "Failed to install dependencies for $MODULE module"
+    fi
+  fi
 }
 
 if [[ ${MODULE} == "zsh" ]]; then
-	ZSH="$(find_shell zsh)"
-	test "$(expr "$SHELL" : '.*/\(.*\)')" != "ZSH" \
-		&& command -v chsh >/dev/null 2>&1 \
-		&& chsh -s "$ZSH" \
-		&& success "set $("$ZSH" --version) at $ZSH as default shell"
+  ZSH="$(find_shell zsh)"
+  test "$(expr "$SHELL" : '.*/\(.*\)')" != "ZSH" &&
+    command -v chsh >/dev/null 2>&1 &&
+    chsh -s "$ZSH" &&
+    success "set $("$ZSH" --version) at $ZSH as default shell"
 fi
 
 if [[ $INSTALL == true ]]; then
-	install_module_dependencies
+  install_module_dependencies
 fi
 
 update_module_config
--- qmk/install.sh
+++ qmk/install.sh
@@ -8,6 +8,8 @@ else
   path+=(/usr/bin/pip3)
 fi
 
+echo "$1"
+
 if [[ ! -d "$HOME/python3/envs/qmk" ]]; then
   mkdir -p "$HOME"/python3/envs
   cd "$HOME"/python3/envs || exit
@@ -19,7 +21,20 @@ fi
 
 cd "$HOME"/python3/envs/qmk || exit
 source ./bin/activate
-qmk setup -H "$PROJECTS/qmk_firmware" Issafalcon/qmk_firmware
+
+# Use command line argument to detemine whether to use vial
+if [[ "$1" == "vial" ]]; then
+  qmk setup -H "$PROJECTS/vial-qmk" -b vial vial-kb/vial-qmk
+  cd "$PROJECTS/vial-qmk" || exit
+  git submodule update --init --recursive
+else
+  qmk setup -H "$PROJECTS/qmk_firmware" qmk/qmk_firmware
+fi
+
 qmk config user.keyboard=keebart/sofle_choc_pro
 qmk config user.keymap=issafalcon
+
+git clone https://github.com/Issafalcon/qmk_userspace.git "${PROJECTS}/qmk_userspace"
+qmk config user.overlay_dir="$(realpath "${PROJECTS}/qmk_userspace")"
+
 deactivate
