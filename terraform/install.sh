91040d74de8a6659f6561de8f94a60ff9592b35d
issafalcon <atfiggins@gmail.com>
2025-07-29 22:50:58 +0100

    Terraform - Updates install script

--- terraform/install.sh
+++ terraform/install.sh
@@ -3,11 +3,18 @@ SCRIPT_DIR=$(cd ${0%/*} && pwd -P)
 
 sudo apt update && sudo apt install -y gnupg software-properties-common curl
 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+wget -O- https://apt.releases.hashicorp.com/gpg |
+  gpg --dearmor |
+  sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg >/dev/null
 
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install -y terraform
+gpg --no-default-keyring \
+  --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+  --fingerprint
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+sudo apt-get install terraform
 
 # Terragrunt install
 if command -v brew >/dev/null; then
