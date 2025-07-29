0713a837edcf244e614e8c9c58ab62e6afb5ff4e
Adam Figgins <adam_figgins@waters.com>
2025-07-29 14:17:02 +0100

    Terraform - Adding mise install

--- terraform/install.sh
+++ terraform/install.sh
@@ -18,3 +18,11 @@ fi
 
 brew install terragrunt
 terragrunt --install-autocomplete
+
+## Mise install (currently required to build terragrunt-ls)
+sudo apt update -y && sudo apt install -y gpg sudo wget curl
+sudo install -dm 755 /etc/apt/keyrings
+wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+sudo apt update
+sudo apt install -y mise
