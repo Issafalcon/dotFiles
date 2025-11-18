2ec191df84344be586956c280699181b0c29a51e
Adam Figgins <adam_figgins@waters.com>
2025-11-18 15:29:37 +0000

    Drawio - Updating version

--- drawio/install.sh
+++ drawio/install.sh
@@ -1,2 +1,2 @@
-sudo curl -Lo /usr/bin/drawio https://github.com/jgraph/drawio-desktop/releases/download/v24.7.17/drawio-x86_64-24.7.17.AppImage
+sudo curl -Lo /usr/bin/drawio https://github.com/jgraph/drawio-desktop/releases/download/v28.2.5/drawio-x86_64-28.2.5.AppImage
 sudo chmod 777 /usr/bin/drawio
