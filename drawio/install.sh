a7f03a56e6c8816c8be9b6ac3d23e9fa926cddb1
issafalcon <atfiggins@gmail.com>
2026-03-09 16:56:10 +0000

    Updating drawio version

--- drawio/install.sh
+++ drawio/install.sh
@@ -1,2 +1,2 @@
-sudo curl -Lo /usr/bin/drawio https://github.com/jgraph/drawio-desktop/releases/download/v28.2.5/drawio-x86_64-28.2.5.AppImage
+sudo curl -Lo /usr/bin/drawio https://github.com/jgraph/drawio-desktop/releases/download/v29.6.1/drawio-x86_64-29.6.1.AppImage
 sudo chmod 777 /usr/bin/drawio
