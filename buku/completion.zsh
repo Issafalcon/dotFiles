702eaf1647295a0b559f8ab330ba86c202c85fa2
Adam Figgins <adam_figgins@waters.com>
2026-03-25 08:48:39 +0000

    buku - Fixing completion snippet loading issue

--- buku/completion.zsh
+++ buku/completion.zsh
@@ -1,1 +1,4 @@
+# Directly add single file completion snippets to zinit, without the need for a plugin repository. This is useful for small snippets that don't require a full plugin structure.
+# https://github.com/zdharma-continuum/zinit?tab=readme-ov-file#use-zi-ice-ascompletion-to-directly-add-single-file-completion-snippets
+zinit ice as"completion"
 zinit snippet https://github.com/jarun/buku/blob/master/auto-completion/zsh/_buku
