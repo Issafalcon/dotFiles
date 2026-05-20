0104f569cd5cbd05a4b2d8fdeb0e3041be77466d
Adam Figgins <adam_figgins@waters.com>
2026-05-20 14:51:04 +0100

    Pandoc - Conversion to pdf required additional latex packages

--- pandoc/install.sh
+++ pandoc/install.sh
@@ -3,3 +3,10 @@
 sudo apt update -y &&
   sudo apt install -y \
     pandoc
+
+# For pandoc to pdf conversion, we need to install the following packages
+# This will install pdflatex and required fonts
+sudo apt-get install texlive-latex-base
+sudo apt-get install texlive-fonts-recommended
+sudo apt-get install texlive-fonts-extra
+sudo apt-get install texlive-latex-extra
